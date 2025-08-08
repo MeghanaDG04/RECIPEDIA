472ace69ecf6295b8a33f99e54912b7ff6644560
satyajeetch1991 <chavansatyajeet2004@gmail.com>
2025-08-08 17:20:22 +0530

    solved merge conflict

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 import { Eye, EyeOff, Mail, Lock, AlertCircle, CheckCircle, Loader2 } from "lucide-react";
